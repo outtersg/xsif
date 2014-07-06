7d8c4b43777f557c9e43889879c51d585a7ac27e
Guillaume Outters <guillaume-xsif@outters.eu>
2014-07-06 22:51:57 +0200

    auteurisation
    = Il fallait quand même bien dire que c'est de moi, non?
    
    darcs-hash:b0d010d483d4e7ec2f4ddda55540f19e2fc6cd4a

--- xsif.php
+++ xsif.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * Copyright (c) 2014 Guillaume Outters
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
 /* xsif
  * Représentation en diagrammes d'un .xsd
  * xs comme XML Schema, et if pour exprimer la représentation en arbre, de façon élégante et compacte (parce que l'if est, de tous les arbres que je connaisse, celui qui possède le nom le plus concis, et aux couleurs raffinées vert sombre et rouge délicat, ainsi que marron et ocre pour le tronc. Et puis il y en a dans le jardin de mes parents et j'aime bien).
